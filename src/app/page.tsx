d1c6eb860604d73baa7962355989a8383517aa51
unknown <sherrera@ad3soft.com>
2025-08-07 22:31:08 -0400

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           <div className="absolute inset-0 bg-black opacity-30"></div>
           <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
             <h1 className="text-white text-4xl md:text-6xl font-bold tracking-tight mb-4">
-              Redefine tu experiencia de viaje
+              Redefine tu experiencia de viajee
             </h1>
             <p className="text-white text-xl md:text-2xl mb-8 max-w-2xl">
               Tu oficina de ultra lujo o tu espacio de relajación personal.
