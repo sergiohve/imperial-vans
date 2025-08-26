7bd045dcdc212c0de494dd07a40de014d2fdc364
unknown <sherrera@ad3soft.com>
2025-08-25 21:13:23 -0400

    cambios

--- .next/static/chunks/src_app_conversiones-lujo_page_tsx_45a68a45._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_11147c84._.js",
-  "static/chunks/src_app_2b54c5df._.js"
-],
-    source: "dynamic"
-});
