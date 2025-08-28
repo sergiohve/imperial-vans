cf64d02432f3f1c20902170c2d68a89f72b2e195
unknown <sherrera@ad3soft.com>
2025-08-28 11:10:24 -0400

    cambios

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QZZeu+AqfovCuz2MYqR4G2/IV9SFq3kQajCDQRip3AM=\"\n}"
