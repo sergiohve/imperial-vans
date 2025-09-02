8fe4996a369e3359207c07c41943f1ab6ac5995d
unknown <sherrera@ad3soft.com>
2025-09-01 21:08:54 -0400

    ca

--- .next/static/chunks/src_app_detail-cars_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_b467cde0._.js",
-  "static/chunks/node_modules_32f3f03f._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_acc22ecc._.js",
-  "static/chunks/node_modules_42ab0a5d._.js"
-],
-    source: "dynamic"
-});
